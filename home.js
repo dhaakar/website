2ffcf12b5b55eb21b46c3dabb67c2981b86ccacf
Sudocker <sudhakar82@gmail.com>
2023-01-29 15:49:44 +0530

    Initial commit with simple homepage

---
+++ home.js
@@ -0,0 +1,31 @@
+function dig(element) {
+  var times = parseInt(element.getAttribute("data-times") || 0);
+  if (times === 5) {
+    poof("dig");
+    reveal("solution");
+    reveal("hero");
+    setTimeout(function () {
+      reveal("stealth");
+    }, 2000);
+  } else {
+    var newText = "˙˙" + element.textContent + "˙˙";
+    element.textContent = newText;
+    element.setAttribute("data-times", ++times);
+  }
+}
+
+function poof(elementId) {
+  var classes = document.getElementById(elementId).classList;
+  classes.add("poof");
+  classes.remove("reveal");
+}
+
+function reveal(elementId) {
+  var classes = document.getElementById(elementId).classList;
+  classes.remove("hide");
+  classes.add("reveal");
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  reveal("problem");
+});
